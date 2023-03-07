9bd1e1d3acd34e5595d48cf1f5bdc51a966feaf1
Kishanvaghasiya108 <122118895+Kishanvaghasiya108@users.noreply.github.com>
2023-03-07 23:12:33 +0530

    f updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useRef } from 'react';
 /**
  * @task :add validation to email, if email is not valid, if not valid email, dont allow to submit
@@ -12,42 +11,41 @@ function App() {
  /**
   * code here
   */
- const fnameRef = useRef();
- const emailRef = useRef();
- const [Error,setError] = useState(undefined);
- const [data,setData] = useState({fname:undefined,lname:undefined});
+   const fnameRef = useRef();
+  const emailRef = useRef();
+  const [error, setError] = useState(undefined);
+  const [data, setData] = useState({ fname: undefined, lname: undefined });
 
-const change = () =>{
-  console.log(emailRef.current.value);
-  if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-za-z]+$/.test(emailRef.current.value)){
-    setError(undefined);
-
-  document.getElementById("submit").disabled = false;
-  }
-  else{
-    setError("Email is invalid");
-    document.getElementById("submit").disabled = true;
-  }
-};
+  const change = () => {
+    // console.log(emailRef.current.value);
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(emailRef.current.value)) {
+      setError(undefined);
+      document.getElementById("submit").disabled = false;
+    } else {
+      setError("Email is invalid");
+      document.getElementById("submit").disabled = true;
+    }
+  };
+        
 
   return(
     <div className="App">
       <h1>How About Them Apples</h1>
       <form onSubmit={(e) => {
-        e.preventDefault();
-        setData({
-          fname:fnameRef.current.value,
-          lname:emailRef.current.value });
-      }}>
+          e.preventDefault();
+          setData({
+            fname: fnameRef.current.value,
+            lname: emailRef.current.value
+          });
+        }}>
         <fieldset>
           <label>
             <p>First Name</p>
-            <input id='fname' name="name" ref={fnameRef}/>
+            <input id='fname' name="name"  ref={fnameRef}/>
             <br></br>
             <p>Email</p>
-            <input id='lname' name="name" onChange={change} ref={emailRef}/>
+            <input id='lname' name="name" onChange={change}  ref={emailRef}/>
             {error && <h2 style={{color: 'red'}}>{error}</h2>}
-            {emailError && <p>Email is invalid</p>}
           </label>
         </fieldset>
 
